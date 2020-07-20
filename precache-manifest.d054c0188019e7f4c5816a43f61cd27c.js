7b88ddd27a73652f6f62237cdcfa67c742fee59d
carendivinagracia <carendivinagracia22@gmail.com>
2020-07-20 14:59:38 +0800

    Updates

---
+++ precache-manifest.d054c0188019e7f4c5816a43f61cd27c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53d0d5b5ed20413e31ab7f7f340e333",
+    "url": "/simple-agile/index.html"
+  },
+  {
+    "revision": "bb3c5cd45a964978f9c8",
+    "url": "/simple-agile/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dff6f4f258345d628ef0",
+    "url": "/simple-agile/static/js/2.b0c3966e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-agile/static/js/2.b0c3966e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb3c5cd45a964978f9c8",
+    "url": "/simple-agile/static/js/main.4c2a6c49.chunk.js"
+  },
+  {
+    "revision": "59b7c9b0cd2cb6850d26",
+    "url": "/simple-agile/static/js/runtime-main.49e7752f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simple-agile/static/media/logo.5d5d9eef.svg"
+  }
+]);
